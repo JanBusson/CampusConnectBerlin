aa534ab8dd8491dbae3da73564109b7458a17aa8
Jan Busson <janbusson@gmx.de>
2025-06-10 17:20:29 +0200

    added some routes and created an (as of now empty Base HTMTL file for the Templates

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
-#comment
+
 app = Flask(__name__)
 
+#Start page
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return 'Start Window!'
+
+#Login page
+@app.route('/login')
+def login():
+    return 'Login Screen'
+
+#Register page
+@app.route('/register')
+def login():
+    return 'Register Screen'
