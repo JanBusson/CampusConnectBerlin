c5f290c9bc6fc6b4ade166f4468fdb8fe70c1dd9
Jan Busson <janbusson@gmx.de>
2025-06-11 18:11:28 +0200

    implemented render temlates for 404 and 500

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 #für Render Template müssen noch alle notwendigen Templates erstellt werden
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+
+app.config.from_mapping(
+    #Noch nich timplementiert
+    #SECRET_KEY = 'secret_key_just_for_dev_environment',
+    #DATABASE = os.path.join(app.instance_path, 'todos.sqlite'),
+    BOOTSTRAP_BOOTSWATCH_THEME = 'pulse'  # (2.)
+)
+
+bootstrap = Bootstrap5(app)
 
 #Start page
 @app.route('/')
@@ -31,8 +41,8 @@ def matches():
 #Error handling
 @app.errorhandler(404)
 def http_not_found(e):
-    return '404'
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def http_internal_server_error(e):
-    return '500'
+    return render_template('500.html'), 500
