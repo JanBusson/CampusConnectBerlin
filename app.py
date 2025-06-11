2ac91d8b4dd1429179d8cf6fbfad6f1230db56e3
Jan Busson <janbusson@gmx.de>
2025-06-11 17:46:17 +0200

    created some of the basic routes and a base html for testing using the reccources from the class

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+#für Render Template müssen noch alle notwendigen Templates erstellt werden
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,5 +15,24 @@ def login():
 
 #Register page
 @app.route('/register')
-def login():
+def register():
     return 'Register Screen'
+
+#Find Match Page
+@app.route('/findMatch')
+def findMatch():
+    return 'Find Match'
+
+#Matches Page
+@app.route('/Matches')
+def matches():
+    return 'My Matches'
+
+#Error handling
+@app.errorhandler(404)
+def http_not_found(e):
+    return '404'
+
+@app.errorhandler(500)
+def http_internal_server_error(e):
+    return '500'
