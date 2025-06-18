e3008786eaa922183a3179ecc98a94bd7fb917f5
Jan Busson <janbusson@gmx.de>
2025-06-18 14:17:00 +0200

    added login form to login route

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,7 @@
 # ermöglicht durch Erstellen einer Blueprint die Auslagerung der einzelnen Routen
 from flask import Blueprint
 
-# BLuepring Objekt main_bp um denen dann Routen zuzuordnen
+# Bluepring Objekt main_bp um denen dann Routen zuzuordnen
 main_bp = Blueprint('main', __name__)
 
 # Importiert alle Routen
--- routes/route_login.py
+++ routes/route_login.py
@@ -1,6 +1,15 @@
-from flask import render_template
+from flask import Blueprint, render_template
 from . import main_bp
+from forms.form_login import CreateLoginForm
+
+login_bp = Blueprint('login', __name__) 
 
 @main_bp.route('/login')
 def login():
-    return render_template('login.html')
+    form = CreateLoginForm()
+
+    if form.validate_on_submit():
+        # TODO Login prüfen
+        return "Login erfolgreich!"  # Platzhalter
+    
+    return render_template('login.html', form=form)
