fe1530abb097b8ce859cf712e9bd12978e43cd2d
Jan Busson <janbusson@gmx.de>
2025-06-21 16:49:09 +0200

    (der branch ist nicht register sondern Welcome) dementsprechend eine Funktionierende Verknüpfung zwishcen Login und Welcome

--- routes/route_welcome.py
+++ routes/route_welcome.py
@@ -1,13 +1,14 @@
 from flask import render_template, abort
 from . import main_bp
 from dao.user_dao import user_dao
-from forms.form_welcome import CreateStartForm
+from forms.form_welcome import CreateWelcomeForm
 
-@main_bp.route('/welcome/<int:user_id>', methods=['GET'])
+@main_bp.route('/welcome/<int:user_id>', methods=['GET', 'POST'])
 def welcome(user_id):
+    form=CreateWelcomeForm()
     user = user_dao.get_uid(user_id)
     #Welcome Seite mit entsprechendem User
     if user:
-        return render_template('welcome.html', username=user.name)
+        return render_template('welcome.html', form=form, username=user.name)
     else:
         abort(404)
