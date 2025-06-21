39409e7d35e0a77447d3888e9c61e25b06e0e9f5
Jan Busson <janbusson@gmx.de>
2025-06-21 16:06:47 +0200

    regisrierungs route erstellt mit entsprechendme form, aber noch WIP + Template noch TODO

--- routes/route_welcome.py
+++ routes/route_welcome.py
@@ -1,7 +1,13 @@
-from flask import render_template
+from flask import render_template, abort
 from . import main_bp
+from dao.user_dao import user_dao
+from forms.form_welcome import CreateStartForm
 
-@main_bp.route('/welcome/<int:id>')
-def welcome():
-    #parameter temporär
-    return render_template('welcome.html')
+@main_bp.route('/welcome/<int:user_id>', methods=['GET'])
+def welcome(user_id):
+    user = user_dao.get_uid(user_id)
+    #Welcome Seite mit entsprechendem User
+    if user:
+        return render_template('welcome.html', username=user.name)
+    else:
+        abort(404)
