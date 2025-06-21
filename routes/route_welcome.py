3041f21bf5e74b50d5312e454849a363a4f35152
Jan Busson <janbusson@gmx.de>
2025-06-21 15:18:49 +0200

    welcome route erstellt

--- routes/route_welcome.py
+++ routes/route_welcome.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import main_bp
 
-@main_bp.route('/welcome')
+@main_bp.route('/welcome/<int:id>')
 def welcome():
     #parameter temporär
-    return render_template('start.html')
+    return render_template('welcome.html')
