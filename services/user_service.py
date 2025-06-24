0b826120641aa0048f77801a3212fb4b19b3389b
kaanbaskaya <semi.kaan.baskaya@gmail.com>
2025-06-24 14:01:18 +0200

    rename user_service.py

--- services/user_service.py
+++ services/user_service.py
@@ -1,9 +1,10 @@
 from datetime import date
 
+# Jan
 def calculate_age(birth_date):
     today = date.today()
-    
-    #liefert das aktuelle Alter
+
+    # liefert das aktuelle Alter
     # erstmal die DIfferenz von aktuellem Jahr und Geburtsjahr, anschließend wird geschaut ob der User dieses Jahr "schon Geburtstag hatte"
     # ist das aktuelle Datum vor dem Geburtstag, so wird 1 abgezogen (weil True, das ergebnis des Vergleichs, als 1 interpretiert wird)
     # wenn der Geburtstag shcon war bleibt das Ergebnis gleich (weil der Vergleich False leifert, was in Python als 0 interpretiert wird)
