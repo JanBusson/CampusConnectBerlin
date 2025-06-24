2199c7098e7dac325b3300bffa4aa8fa8cb6bcd2
Jan Busson <janbusson@gmx.de>
2025-06-24 11:33:59 +0200

    Verknüpfung von Welcome zu Matching

--- routes/route_matching.py
+++ routes/route_matching.py
@@ -3,7 +3,9 @@ from . import main_bp
 from flask import render_template
 from dao.user_dao import user_dao
 
-@main_bp.route('/matching', methods=['GET', 'POST']) 
+@main_bp.route('/matching/<int:user_id>', methods=['GET', 'POST']) 
 def matching(user_id):
      user= user_dao.get_uid(user_id=user_id)
+
+
      return render_template('matching.html', user=user)
--- routes/route_register.py
+++ routes/route_register.py
@@ -1,4 +1,4 @@
-from flask import render_template, abort
+from flask import render_template,redirect, url_for
 from . import main_bp
 from dao.university_dao import university_dao
 from dao.user_dao import user_dao
@@ -13,6 +13,7 @@ def register():
     if form.validate_on_submit():
     # Auslesen der Eingaben
         name = form.name.data
+        #TODO email unique checken
         email = form.email.data
         university_id = form.university.data
         birthday = form.birthday.data
@@ -20,6 +21,10 @@ def register():
         hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         profile_picture_data=form.profilePic.data
         profile_picture= profile_picture_data.read()
-        user_dao.create_user(name=name,email=email,password=hashed_pw,uni_id=university_id,birth_date=birthday,profile_picture=profile_picture)
+
+        #den neune Nutzer mit den Eingaben anlegen
+        new_user = user_dao.create_user(name=name,email=email,password=hashed_pw,uni_id=university_id,birth_date=birthday,profile_picture=profile_picture)
+
+        return redirect(url_for('main.welcome',user_id=new_user.user_id))
 
     return render_template('register.html', form=form)
