08547a0ee8ff2fa81e8379114298cdabb2d7f6c5
kaanbaskaya <s_baskaya23@stud.hwr-berlin.de>
2025-07-02 22:44:12 +0200

    max distanz auf -3 bis 3 geändert

--- services/match_service.py
+++ services/match_service.py
@@ -12,5 +12,5 @@ class MatchService:
             (result1.vec_tf - result2.vec_tf) ** 2 +
             (result1.vec_jp - result2.vec_jp) ** 2
         )
-        max_distance = math.sqrt(4 * (5 ** 2))
+        max_distance = math.sqrt(4 * (6 ** 2))
         return round(1 - (diff / max_distance), 2)
