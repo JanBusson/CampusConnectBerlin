10a3651f7fad0e2cb637b030b85428ba07a05d7f
Jan Busson <janbusson@gmx.de>
2025-07-02 13:52:27 +0200

    Einbezug von Session für Register/login

--- routes/route_register.py
+++ routes/route_register.py
@@ -1,4 +1,4 @@
-from flask import render_template,redirect, url_for
+from flask import render_template,redirect, url_for, session
 from . import main_bp
 from dao.university_dao import university_dao
 from dao.user_dao import user_dao
@@ -25,7 +25,7 @@ def register():
 
         #den neune Nutzer mit den Eingaben anlegen
         new_user = user_dao.create_user(name=name,email=email,password=hashed_pw,uni_id=university_id,birth_date=birthday,profile_picture=profile_picture,description=description)
-
+        session['user_id']=new_user.user_id
         return redirect(url_for('main.welcome',user_id=new_user.user_id))
 
     return render_template('register.html', form=form)
