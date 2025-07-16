6360c21df33ba11f39f8c5946f9693b6f4246065
kaanbaskaya <s_baskaya23@stud.hwr-berlin.de>
2025-07-16 19:23:13 +0200

    max_distance als Konstante

--- services/match_service.py
+++ services/match_service.py
@@ -12,5 +12,6 @@ class MatchService:
             (result1.vec_tf - result2.vec_tf) ** 2 +
             (result1.vec_jp - result2.vec_jp) ** 2
         )
-        max_distance = math.sqrt(4 * (6 ** 2))
+        # max_distance = math.sqrt(4 * (6 ** 2))
+        max_distance = 12 # in 15 die Rechnung, weil es aber eine Konstante ist → direkt auf =12 gesetzt
         return round(1 - (diff / max_distance), 2)
